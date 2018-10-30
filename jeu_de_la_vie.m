0339d1e361dba25714e617903d006d42f1de0c7a
lecart alan <alan.lecart@outlook.com>
2018-10-30 12:12:51 +0100

    Reste a debug les solos

--- jeu_de_la_vie.m
+++ jeu_de_la_vie.m
@@ -1,28 +1,30 @@
 function jeu = jeu_de_la_vie
 
     jeu=1;
-    taille = 10;
+    taille = 50;
     game = remplir_aleatoire(taille);
-%         game = zeros(taille);
-%         game(5,5)= 1;
-%         game(5,6)= 1;
-%         game(5,7)= 1;
-    tps_pause = 2;
+%     game = zeros(taille);
+%     game(5,5)= 1;
+%     game(5,6)= 1;
+%     game(5,7)= 1;
+    tps_pause = 0.1;
 
     while (1)
-        spy(game);
+        copy = game;
+        spy(copy);
         pause(tps_pause);
         for i = 1:taille
             for j = 1:taille
 
-                if voisin(game,i,j,taille)== 2 || voisin(game,i,j,taille)== 3
-                    game(i,j)= vivant();
-                else
-                    game(i,j)= mourrir();
+                if voisin(game,i,j,taille)== 3
+                    copy(i,j)= vivant();
+                elseif voisin(game,i,j,taille)== 1 || voisin(game,i,j,taille) > 3 
+                    copy(i,j)= mourrir();
                 end
 
             end
         end
+        game = copy;
     end
     
 end
